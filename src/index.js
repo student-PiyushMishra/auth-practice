7c7691ed3f5af4206a3bf1ee6081ed6015d94302
Piyush Mishra <student.piyushmishra@gmail.com>
2025-06-09 13:20:32 +0530

    added the config for ejs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import ejs from "ejs";
+import path from "path"
+import __dirname from "../staticFiles.js"
+import staticRoutes from "./routes/staticRoute.js"
 
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT;
 
-console.log(port)
+app.set('view engine', "ejs");
+app.use(express.static(path.join(__dirname,"public")))
+
+app.use("/",staticRoutes)
 
 app.listen(port, ()=>{
     console.log(`Server is up and listening on PORT: ${port}`);
